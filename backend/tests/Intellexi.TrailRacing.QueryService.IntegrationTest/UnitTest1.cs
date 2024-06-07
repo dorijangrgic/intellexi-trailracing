d1592e4f3ccee8968b30e37cdbc89db7152762df
dorijangrgic <grgic.dorijan@gmail.com>
2024-06-07 19:17:40 +0200

    Add both backend and frontend solutions. Add docker-compose.yml

---
+++ backend/tests/Intellexi.TrailRacing.QueryService.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Intellexi.TrailRacing.QueryService.IntegrationTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
